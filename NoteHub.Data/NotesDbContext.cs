f4bc6c58ebf6a8b1a9e229bf241e6cc7cb655384
nikicha2209 <nikicha08@gmail.com>
2025-02-05 16:44:13 +0200

    Created DB context

---
+++ NoteHub.Data/NotesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteHub.Data.Models;
+
+namespace NoteHub.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
