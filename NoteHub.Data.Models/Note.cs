bf887fafd86c3ae704ba2d0f4f0db52a7c31a2ab
nikicha2209 <nikicha08@gmail.com>
2025-02-05 14:19:44 +0200

    Created model Note

---
+++ NoteHub.Data.Models/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace NoteHub.Data.Models
+{
+    public class Note
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+    }
+}
