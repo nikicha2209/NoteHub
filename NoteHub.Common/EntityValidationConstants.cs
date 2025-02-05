f62eb05d997d567bde74cbfdd3e3d3aa6b5fa98a
nikicha2209 <nikicha08@gmail.com>
2025-02-05 16:43:23 +0200

    Added EntityConstants

---
+++ NoteHub.Common/EntityValidationConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace NoteHub.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Note
+        {
+            public const int NoteTitleMinLength = 3;
+            public const int NoteTitleMaxLength = 100;
+
+            public const int NoteContentMinLength = 10;
+            public const int NoteContentMaxLength = 10000;
+        }
+    }
+}
