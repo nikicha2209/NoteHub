4d0aaa8e8154058525389088df15622db777abbf
nikicha2209 <nikicha08@gmail.com>
2025-02-05 19:47:53 +0200

    Fixed EntityValidationMessages

--- NoteHub.Common/EntityValidationMessages.cs
+++ NoteHub.Common/EntityValidationMessages.cs
@@ -8,8 +8,8 @@
             public const string NoteTitleRequiredMessage = "Title is required.";
             public const string NoteContentRequiredMessage = "Content is required.";
 
-            public const string NoteTitleLengthMessage = "Title must be between {0} and {1} characters.";
-            public const string NoteContentLengthMessage = "Content must be between {0} and {1} characters.";
+            public const string NoteTitleLengthMessage = "Title must be between {2} and {1} characters.";
+            public const string NoteContentLengthMessage = "Content must be between {2} and {1} characters.";
         }
        
     }
