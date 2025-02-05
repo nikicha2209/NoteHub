5517ee79994ce80f8702022c5a24723bff909507
nikicha2209 <nikicha08@gmail.com>
2025-02-05 17:37:49 +0200

    Added BaseService

---
+++ NoteHub.Services.Data/BaseService.cs
@@ -0,0 +1,24 @@
+﻿using NoteHub.Services.Data.Interfaces;
+
+namespace NoteHub.Services.Data
+{
+    public class BaseService : IBaseService
+    {
+        public bool IsGuidValid(string? id, ref Guid parsedGuid)
+        {
+            // Non-existing parameter in the URL
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                return false;
+            }
+
+            // Invalid parameter in the URL
+            bool isGuidValid = Guid.TryParse(id, out parsedGuid);
+            if (!isGuidValid)
+            {
+                return false;
+            }
+
+            return true;
+        }
+}
