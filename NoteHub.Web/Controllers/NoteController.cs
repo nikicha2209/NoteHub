739db271a86e95bbfee53ab355d23afc20bfaea7
nikicha2209 <nikicha08@gmail.com>
2025-02-05 19:53:09 +0200

    Added NoteController to work with the service

---
+++ NoteHub.Web/Controllers/NoteController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoteHub.Services.Data.Interfaces;
+using NoteHub.Web.ViewModels;
+
+namespace NoteHub.Web.Controllers
+{
+    public class NoteController : Controller
+    {
+        private readonly INoteService _noteService;
+
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var notes = await _noteService.GetAllNotes();
+            return View(notes); 
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(NoteViewModel noteViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _noteService.CreateNote(noteViewModel); 
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(noteViewModel); 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var note = await _noteService.GetNoteById(id);
+            if (note == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(note);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, NoteViewModel noteViewModel)
+        {
+            if (id != noteViewModel.Id)
+            {
+                return NotFound(); 
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _noteService.UpdateNote(noteViewModel);
+                return RedirectToAction(nameof(Index)); 
+            }
+
+            return View(noteViewModel); 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var note = await _noteService.GetNoteById(id);
+            if (note == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(note); 
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            await _noteService.DeleteNote(id); 
+            return RedirectToAction(nameof(Index)); 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var note = await _noteService.GetNoteById(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return View(note); 
+        }
+    }
+}
