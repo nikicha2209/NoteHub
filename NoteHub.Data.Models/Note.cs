97d74915b98356aa299d025e80a1222156ed9a53
nikicha2209 <nikicha08@gmail.com>
2025-02-05 14:37:32 +0200

    Added validations to Note model

--- NoteHub.Data.Models/Note.cs
+++ NoteHub.Data.Models/Note.cs
@@ -1,9 +1,20 @@
-﻿namespace NoteHub.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+using static NoteHub.Common.EntityValidationConstants.Note;
+
+namespace NoteHub.Data.Models
 {
     public class Note
     {
+        [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(NoteTitleMaxLength)]
         public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(NoteContentMaxLength)]
         public string Content { get; set; } = null!;
     }
 }
