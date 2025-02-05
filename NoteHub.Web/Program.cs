f8844a5a21e08d0133e59cecf464818e7ebad98c
nikicha2209 <nikicha08@gmail.com>
2025-02-05 17:33:37 +0200

    Updated Program.cs file

--- NoteHub.Web/Program.cs
+++ NoteHub.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NoteHub.Data;
+
 namespace NoteHub.Web
 {
     public class Program
@@ -5,6 +8,9 @@ namespace NoteHub.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<NotesDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));  
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
