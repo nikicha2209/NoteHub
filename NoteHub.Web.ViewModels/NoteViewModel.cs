44bb3ec3af85c33167d2ebe84ea0cca78cfa43e0
nikicha2209 <nikicha08@gmail.com>
2025-02-05 18:15:52 +0200

    Created NoteViewModel

---
+++ NoteHub.Web.ViewModels/NoteViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static NoteHub.Common.EntityValidationMessages.Note;
+using static NoteHub.Common.EntityValidationConstants.Note;
+
+namespace NoteHub.Web.ViewModels
+{
+    public class NoteViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = NoteTitleRequiredMessage)]
+        [StringLength(NoteTitleMaxLength,
+            MinimumLength = NoteTitleMinLength,
+            ErrorMessage = NoteTitleLengthMessage)]
+        public string Title { get; set; } = null!;
+
+
+        [Required(ErrorMessage = NoteContentRequiredMessage)]
+        [StringLength(NoteContentMaxLength,
+            MinimumLength = NoteContentMinLength,
+            ErrorMessage = NoteContentLengthMessage)]
+        public string Content { get; set; } = null!;
+    }
+}
