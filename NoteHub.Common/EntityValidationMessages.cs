b8373e046603a8ec0fd8f3418bad891ec04c6b12
nikicha2209 <nikicha08@gmail.com>
2025-02-05 18:15:32 +0200

    Created EntityValidationMessages

---
+++ NoteHub.Common/EntityValidationMessages.cs
@@ -0,0 +1,16 @@
+﻿namespace NoteHub.Common
+{
+    public static class EntityValidationMessages
+    {
+
+        public static class Note
+        {
+            public const string NoteTitleRequiredMessage = "Title is required.";
+            public const string NoteContentRequiredMessage = "Content is required.";
+
+            public const string NoteTitleLengthMessage = "Title must be between {0} and {1} characters.";
+            public const string NoteContentLengthMessage = "Content must be between {0} and {1} characters.";
+        }
+       
+    }
+}
